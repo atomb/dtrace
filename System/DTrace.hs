4f8a9bc7b702b6ec2577a952f8f854e0e9cfde9c
Spiridon Eliopoulos <seliopou@gmail.com>
2009-07-24 23:03:20 -0400

    Build on non-darwin systems
    ... so traces can be left in source code without preventing people on
    non-darwin systems from building.

--- System/DTrace.hs
+++ System/DTrace.hs
@@ -1,12 +1,12 @@
-{-# LANGUAGE ForeignFunctionInterface #-}
-
-{-# CFILES cbits/user_trace.c #-}
-
+{-# LANGUAGE ForeignFunctionInterface, CPP #-}
 module System.DTrace (trace) where
+
+#ifdef ENABLE_TRACE
 
 import Control.Monad ( when )
 import Foreign.C.String
 
+{-# CFILES cbits/user_trace.c #-}
 foreign import ccall "trace.h user_trace" user_trace :: CString -> IO ()
 foreign import ccall "trace.h user_trace_enabled" user_trace_enabled :: IO Bool
 
@@ -15,3 +15,10 @@ trace msg = do
     enabled <- user_trace_enabled
     when enabled
         (withCString msg user_trace)
+
+#else
+
+trace :: String -> IO ()
+trace _ = return ()
+
+#endif
