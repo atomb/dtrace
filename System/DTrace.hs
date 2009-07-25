c150ee522bc363b458d9645fa69f2d864a257161
Spiridon Eliopoulos <seliopou@gmail.com>
2009-07-24 20:55:20 -0400

    Check that the probe is enabled from Haskell
    Rather than unconditionally marshalling the trace string, first check to see
    if the probe is enabled. Only then do we fire the probe. This approach incurs
    a little more memory usage when tracing is enabled, but is much faster and
    memory efficient when tracing is disabled.

--- System/DTrace.hs
+++ System/DTrace.hs
@@ -4,9 +4,14 @@
 
 module System.DTrace (trace) where
 
+import Control.Monad ( when )
 import Foreign.C.String
 
 foreign import ccall "trace.h user_trace" user_trace :: CString -> IO ()
+foreign import ccall "trace.h user_trace_enabled" user_trace_enabled :: IO Bool
 
 trace :: String -> IO ()
-trace msg = withCString msg user_trace
+trace msg = do
+    enabled <- user_trace_enabled
+    when enabled
+        (withCString msg user_trace)
--- cbits/user-trace.c
+++ cbits/user-trace.c
@@ -1,7 +1,13 @@
 #include "trace.h"
 
 void user_trace(char *arg) {
-    if(HASKELL_USER_TRACE_ENABLED()) {
-        HASKELL_USER_TRACE(arg);
+    HASKELL_USER_TRACE(arg);
+}
+
+int user_trace_enabled() {
+    if (HASKELL_USER_TRACE_ENABLED()) {
+        return 1;
+    } else {
+        return 0;
     }
 }
